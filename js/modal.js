7b07771e59f704f288b2556fcd8c4b779bc73f93
teclevel <szaytsev060576@gmail.com>
2020-10-23 00:08:53 +0300

    оживление модального окна

---
+++ js/modal.js
@@ -0,0 +1,59 @@
+const modalOpen = document.querySelector('.contact-button');
+const letterPopup = document.querySelector('.modal-contact');
+const modalClose = letterPopup.querySelector('.modal-close');
+const letterForm = letterPopup.querySelector('.fill-form');
+const userName = letterPopup.querySelector('.user-name');
+const userEmail = letterPopup.querySelector('.user-email');
+const userLetter = letterPopup.querySelector('.user-letter');
+
+let isStorageSupport = true;
+let storage = '';
+
+try {
+  storage = localStorage.getItem('user-name');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+modalOpen.addEventListener('click', function(evt){
+  evt.preventDefault();
+  letterPopup.classList.add('modal-open');
+
+  if(storage){
+    userName.value = storage;
+    userEmail.focus();
+  } else {
+    userName.focus();
+    }
+});
+
+
+modalClose.addEventListener('click', function(evt){
+    letterPopup.classList.remove('modal-open');
+    letterPopup.classList.remove('modal-error');
+});
+
+
+letterForm.addEventListener('submit', function (evt) {
+    if(!userName.value || !userEmail.value || !userLetter.value){
+      evt.preventDefault();
+      letterPopup.classList.remove('modal-error');
+      letterPopup.offsetWidth = letterPopup.offsetWidth;
+      letterPopup.classList.add('modal-error');
+    } else {
+      if(isStorageSupport){
+        localStorage.setItem('user-name', userName.value);
+      }
+  }
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (letterPopup.classList.contains('modal-open')) {
+      evt.preventDefault();
+      letterPopup.classList.remove('modal-open');
+      letterPopup.classList.remove('modal-error');
+    }
+  }
+});
